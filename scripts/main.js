480b359f77ec63fc316aa89c41170c6a501298ca
meyerkn00 <karl@themeyers.org>
2023-10-23 12:22:33 -0400

    More exploration

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+#/let myVariable = "Bob"; #/
+
+#/
+document.querySelector("html").addEventListener("click", function () {
+  alert("Ouch! Stop poking me!");
+});
+#/
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
