a3a92766e27a54f2656cad3c076977711f119f0b
meyerkn00 <karl@themeyers.org>
2023-10-23 15:29:01 -0400

    Centering and Javascript debugging
    trying to center the 'calculate' button is a difficult task, as is getting the javascript to load an event listener

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,23 @@
 function formsubmit(event) {
-  var weight = document.getElementById("weight");
-  var volgal = document.getElementById("volgoal");
-  /* add additional form elements */
+    var weight = document.getElementById("weight");
+    var volgal = document.getElementById("volgoal");
+    /* add additional form elements */
 
-  localStorage.setItem("weight", weight.value)
-  localStorage.setItem("volgoal", volgoal.value)
-  /* add additional variables to save locally */
-  
-  /* math goes here*/
+    localStorage.setItem("weight", weight.value)
+    localStorage.setItem("volgoal", volgoal.value)
+    /* add additional variables to save locally */
+    
+    /* math goes here*/
 
-  /* below is where I output the results in a <p> element */
-  output.textContent = 'Output goes here:';
+    /* below is where I output the results in a <p> element */
+    output.textContent = 'Output goes here';
+  }
+
+window.onload = (event) => {
+
+  const form = document.getElementById("form");
+  const output = document.getElementById("output");
+  /* Form submission is an event, so I can use that to call my function */
+  form.addEventListener("submit", formsubmit);
+
 }
-
-const form = document.getElementById("form");
-const output = document.getElementById("output");
-/* Form submission is an event, so I can use that to call my function */
-form.addEventListener("submit", formsubmit);
