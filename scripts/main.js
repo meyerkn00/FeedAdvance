594a4c8e54be724a2e72d453065be4d4cb60292c
meyerkn00 <karl@themeyers.org>
2023-10-24 11:35:59 -0400

    Documenting JS and testing math
    Right now the + operator concatenates instead of summing, trying to convert to number with + operator

--- scripts/main.js
+++ scripts/main.js
@@ -2,18 +2,24 @@ function formsubmit(event) {
     /* This one line stops the form from actually submitting, and instead lets JS hijack it */
     event.preventDefault();
 
+    /* declare each form element so that JS can call it. This step does not store the data */
     var weight = document.getElementById("weight");
     var volgal = document.getElementById("volgoal");
-    /* add additional form elements */
+    /* add additional form elements here*/
 
+    /* take form results and store them as variables locally */
     localStorage.setItem("weight", weight.value)
     localStorage.setItem("volgoal", volgoal.value)
     /* add additional variables to save locally */
-    
-    /* math goes here*/
 
-    /* below is where I output the results in a <p> element */
-    output.textContent = 'Output goes here';
+    /* math goes here */
+    let result = +weight.value + +volgoal.value
+
+    /* 
+    Below is where I output the results in the <p> element directly below the form. 
+    Note that concat string needs to go within backticks
+    */
+    output.textContent = `Output goes here: ${result}`;
 }
 
 window.onload = function() {
