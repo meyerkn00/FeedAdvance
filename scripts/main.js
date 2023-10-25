2ec9212fe964c1c0b38a50bf762175604d90d38b
meyerkn00 <karl@themeyers.org>
2023-10-25 12:21:23 -0400

    Keep old JS for now
    New query selector didn't work so I wanted to keep the existing code since it is functional

--- scripts/main.js
+++ scripts/main.js
@@ -2,20 +2,35 @@ function formsubmit(event) {
     /* This one line stops the form from actually submitting, and instead lets JS hijack it */
     event.preventDefault();
 
-    /* Pulls key-value pairs from submitted form */
+    /* Currently broken: Pulls key-value pairs from submitted form
     let form = document.querySelector("form")
 
-    /* Takes said key-value pairs and assigns to variables we can use */
-    let weight = form.elements.weightvol
+    Takes said key-value pairs and assigns to variables we can use
+    let weight = form.elements.weightval
     let volgoal = form.elements.volgoalval
     let thing1 = form.elements.thing1val
     let thing2 = form.elements.thing2val
+    */
+
+    /* declare each form element so that JS can call it. This step does not store the data */
+    var weight = document.getElementById("weight");
+    var volgal = document.getElementById("volgoal");
+    var volgal = document.getElementById("thing1");
+    var volgal = document.getElementById("thing2");
+    /* add additional form elements here*/
+
+    /* take form results and store them as variables locally */
+    localStorage.setItem("weight", weight.value)
+    localStorage.setItem("volgoal", volgoal.value)
+    localStorage.setItem("thing1", thing1.value)
+    localStorage.setItem("thing2", thing2.value)
+    /* add additional variables to save locally */
 
     /* 
     math goes here 
     to do: coerce thing1/2 values from null to 0 if they are empty
     */
-    let result = +weight + +volgoal
+    let result = +weight.value + +volgoal.value
 
     /* 
     Below is where I output the results in the <p> element directly below the form. 
