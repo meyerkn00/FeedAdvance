f65abbbcf4e36ae3d5e1f9157f6b3963a2e062d7
meyerkn00 <karl@themeyers.org>
2023-10-25 12:14:26 -0400

    Use submitted for key-value pairs
    This is currently broken, but working on it

--- scripts/main.js
+++ scripts/main.js
@@ -2,25 +2,20 @@ function formsubmit(event) {
     /* This one line stops the form from actually submitting, and instead lets JS hijack it */
     event.preventDefault();
 
-    /* declare each form element so that JS can call it. This step does not store the data */
-    var weight = document.getElementById("weight");
-    var volgal = document.getElementById("volgoal");
-    var volgal = document.getElementById("thing1");
-    var volgal = document.getElementById("thing2");
-    /* add additional form elements here*/
+    /* Pulls key-value pairs from submitted form */
+    let form = document.querySelector("form")
 
-    /* take form results and store them as variables locally */
-    localStorage.setItem("weight", weight.value)
-    localStorage.setItem("volgoal", volgoal.value)
-    localStorage.setItem("thing1", thing1.value)
-    localStorage.setItem("thing2", thing2.value)
-    /* add additional variables to save locally */
+    /* Takes said key-value pairs and assigns to variables we can use */
+    let weight = form.elements.weightvol
+    let volgoal = form.elements.volgoalval
+    let thing1 = form.elements.thing1val
+    let thing2 = form.elements.thing2val
 
     /* 
     math goes here 
     to do: coerce thing1/2 values from null to 0 if they are empty
     */
-    let result = +weight.value + +volgoal.value
+    let result = +weight + +volgoal
 
     /* 
     Below is where I output the results in the <p> element directly below the form. 
