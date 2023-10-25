cbc9e4c8ce4fc719a63e556066e3648b973c52eb
meyerkn00 <karl@themeyers.org>
2023-10-25 14:42:52 -0400

    Updated documentation

--- scripts/main.js
+++ scripts/main.js
@@ -2,19 +2,16 @@ function formsubmit(event) {
     /* This one line stops the form from actually submitting, and instead lets JS hijack it */
     event.preventDefault();
 
+    /* Pulls key-value pair from the form result and stores it as formData */
     const formElement = document.querySelector('form');
     const formData = new FormData(formElement);
 
-    /* 
-    math goes here 
-    to do: coerce thing1/2 values from null to 0 if they are empty
-    
-    let result = +formData.get('weightval') + +formData.get('volgoalval');
-    */
+    /* math goes here */
     let feed_volume = (+formData.get('feedlimit_val') * +formData.get('weight_val'))/8;
 
     let hourly_feed_volume = (feed_volume/3);
 
+    /* Rates are coerced to 0 if they are any "falsey" values such as NaN, null, etc */
     let fluid_rate = +formData.get('tflrate_val') 
       - (formData.get('rate1') || 0)
       - (formData.get('rate2') || 0)
@@ -22,14 +19,19 @@ function formsubmit(event) {
       - (formData.get('rate4') || 0)
       - (formData.get('rate5') || 0)
       - hourly_feed_volume;
+    
     /* 
     Below is where I output the results in the <p> element directly below the form. 
     Note that concat string needs to go within backticks
+    Numbers are rounded to 1 decimal place and turned into string 
+    (important only if I need to do more math)
     */
     output.textContent = `Fluid Rate: ${fluid_rate.toFixed(2)} (mL/hr). 
       Feed Volume: ${feed_volume.toFixed(1)} (ml per 3 hours)`;
 }
 
+
+/* Below waits untilt the page loads and then creates a listener event for form submission */
 window.onload = function() {
 
   var form = document.getElementById("dataform");
