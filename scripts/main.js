5dacb22ee82ee860fe347fda36885ea214e0c07b
meyerkn00 <karl@themeyers.org>
2023-10-25 12:32:19 -0400

    Simplify form data input to JS
    Instead of taking values from the form inputs, I take the key-value pair from the form and use that to get each value. The benefit is its much shorter and does not require I define each variable before I use it

--- scripts/main.js
+++ scripts/main.js
@@ -2,24 +2,30 @@ function formsubmit(event) {
     /* This one line stops the form from actually submitting, and instead lets JS hijack it */
     event.preventDefault();
 
-    /* Currently broken: Pulls key-value pairs from submitted form
-    let form = document.querySelector("form")
+    /* Currently broken: Pulls key-value pairs from submitted form 
+    let form = document.querySelector("form");
 
-    Takes said key-value pairs and assigns to variables we can use
-    let weight = form.elements.weightval
-    let volgoal = form.elements.volgoalval
-    let thing1 = form.elements.thing1val
-    let thing2 = form.elements.thing2val
-    */
+    /* Takes said key-value pairs and assigns to variables we can use 
+    let weight = form.elements.weightval;
+    let volgoal = form.elements.volgoalval;
+    let thing1 = form.elements.thing1val;
+    let thing2 = form.elements.thing2val;
+    */    
 
-    /* declare each form element so that JS can call it. This step does not store the data */
+
+    const formElement = document.querySelector('form');
+    const formData = new FormData(formElement);
+    console.log(formData.get('weightval'));
+
+    /* old code
+    /* declare each form element so that JS can call it. This step does not store the data 
     var weight = document.getElementById("weight");
     var volgal = document.getElementById("volgoal");
     var volgal = document.getElementById("thing1");
     var volgal = document.getElementById("thing2");
     /* add additional form elements here*/
 
-    /* take form results and store them as variables locally */
+    /* take form results and store them as variables locally 
     localStorage.setItem("weight", weight.value)
     localStorage.setItem("volgoal", volgoal.value)
     localStorage.setItem("thing1", thing1.value)
@@ -30,7 +36,7 @@ function formsubmit(event) {
     math goes here 
     to do: coerce thing1/2 values from null to 0 if they are empty
     */
-    let result = +weight.value + +volgoal.value
+    let result = +formData.get('weightval') + +formData.get('volgoalval');
 
     /* 
     Below is where I output the results in the <p> element directly below the form. 
